782a6a3db8878b386a3ee366a8601f9abe11cb18
malshaniwijekoon <malshani.wijekoon@gmail.com>
2023-08-16 11:34:43 +0530

    csv file upload api

--- src/main/java/com/example/springbatch/ServletInitializer.java
+++ src/main/java/com/example/springbatch/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(OnlineBookStoreApplication.class);
+        return application.sources(SpringBatch.class);
     }
 
 }
--- src/main/java/com/example/springbatch/SpringBatch.java
+++ src/main/java/com/example/springbatch/SpringBatch.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineBookStoreApplication {
+public class SpringBatch {
 
     public static void main(String[] args) {
-        SpringApplication.run(OnlineBookStoreApplication.class, args);
+        SpringApplication.run(SpringBatch.class, args);
     }
 
 }
